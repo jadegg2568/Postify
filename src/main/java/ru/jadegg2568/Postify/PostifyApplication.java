539b69f9639aa7c39ebc47d346f52de7d70d3def
jadegg_ <svistunovilya09@gmail.com>
2026-03-22 00:15:19 +0300

    initial commit

---
+++ src/main/java/ru/jadegg2568/Postify/PostifyApplication.java
@@ -0,0 +1,13 @@
+package ru.jadegg2568.Postify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/jadegg2568/Postify/PostifyApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.jadegg2568.Postify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
