1a1999b9768d1f2a19d34ae7233d618c7247be9a
jadegg_ <svistunovilya09@gmail.com>
2026-03-26 12:07:00 +0300

    implement mapstruct in gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,16 +30,36 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.flywaydb:flyway-core")
 	implementation("org.flywaydb:flyway-database-postgresql")
+
+	// Lombok
 	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+
+	// MapStruct
+	implementation("org.mapstruct:mapstruct:1.5.5.Final")
+	annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
+
+	// Lombok + MapStruct integration (важно для совместной работы)
+	annotationProcessor("org.projectlombok:lombok-mapstruct-binding:0.2.0")
+
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("org.postgresql:postgresql")
-	annotationProcessor("org.projectlombok:lombok")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.boot:spring-boot-testcontainers")
 	testImplementation("org.springframework.security:spring-security-test")
 	testImplementation("org.testcontainers:junit-jupiter")
 	testImplementation("org.testcontainers:postgresql")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+// Настройка MapStruct
+tasks.withType<JavaCompile> {
+	options.annotationProcessorPath = configurations.annotationProcessor.get()
+	options.compilerArgs = listOf(
+		"-Amapstruct.defaultComponentModel=spring",
+//		"-Amapstruct.unmappedTargetPolicy=IGNORE"
+	)
 }
 
 tasks.withType<Test> {
