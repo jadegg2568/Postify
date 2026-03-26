8652e13658d60b4415c566c7c9e11fb3fac2b432
jadegg_ <svistunovilya09@gmail.com>
2026-03-26 12:34:39 +0300

    add user entity

---
+++ src/main/java/ru/jadegg2568/Postify/entity/User.java
@@ -0,0 +1,46 @@
+package ru.jadegg2568.Postify.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @UuidGenerator
+    @Column(unique = true, nullable = false, updatable = false)
+    private UUID uuid;
+
+    @Email
+    @Column(unique = true)
+    private String mail;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    @Column(unique = true, nullable = false, length = 24)
+    private String username;
+
+    @Column(length = 32)
+    private String title;
+
+    @Column(length = 256)
+    private String description;
+
+    // S3 image key
+    private String photoKey;
+
+    @CreationTimestamp
+    private Instant createdAt;
+}
