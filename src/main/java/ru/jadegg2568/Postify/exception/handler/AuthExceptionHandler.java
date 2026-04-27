df85962f1b3c91f3e6b4cf31ad9d4ec8c23944f4
jadegg_ <svistunovilya09@gmail.com>
2026-04-27 17:35:44 +0300

    fix(exception): handle when user didn't authorize when it's important

--- src/main/java/ru/jadegg2568/Postify/exception/handler/AuthExceptionHandler.java
+++ src/main/java/ru/jadegg2568/Postify/exception/handler/AuthExceptionHandler.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.jadegg2568.Postify.response.ErrorResponse;
@@ -13,14 +14,15 @@ import ru.jadegg2568.Postify.response.ErrorResponse;
 @RestControllerAdvice
 public class AuthExceptionHandler {
 
-    @ExceptionHandler(JwtException.class)
-    public ResponseEntity<ErrorResponse> handleJwt(JwtException e) {
+    @ExceptionHandler({JwtException.class,
+            InsufficientAuthenticationException.class})
+    public ResponseEntity<ErrorResponse> handleUnauthorized(JwtException e) {
         log.debug("JWT error: {}", e.getMessage());
 
         ErrorResponse error = new ErrorResponse(
                 HttpStatus.UNAUTHORIZED,
-                "UNAUTHORIZED",
-                "Invalid or expired token"
+                "NOT_AUTHORIZED",
+                "Please authorize"
         );
 
         return new ResponseEntity<>(error, error.status());
