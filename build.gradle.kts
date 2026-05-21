b0eaf1ade3c5a8c082a80c872b965981e1e3ceb9
jadegg_ <svistunovilya09@gmail.com>
2026-05-21 18:13:32 +0300

    feat(app): implement Actuators

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,10 +24,14 @@ repositories {
 }
 
 dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
-	implementation("org.springframework.boot:spring-boot-starter-web")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+	// DevOps
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("io.micrometer:micrometer-registry-prometheus")
 
 	// Security
 	implementation("org.springframework.boot:spring-boot-starter-security")
@@ -42,8 +46,6 @@ dependencies {
 	// MapStruct
 	implementation("org.mapstruct:mapstruct:1.5.5.Final")
 	annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
-
-	// Lombok + MapStruct integration
 	annotationProcessor("org.projectlombok:lombok-mapstruct-binding:0.2.0")
 
 	// Databases + Migrations
@@ -54,6 +56,7 @@ dependencies {
 	// Documentation
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.9")
 
+	// Tests
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.boot:spring-boot-testcontainers")
 	testImplementation("org.springframework.security:spring-security-test")
